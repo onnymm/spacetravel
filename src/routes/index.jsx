cf74b003acee0e07ba759b8fb41c6f5db51e49ac
onnymm <phr2807@gmail.com>
2024-04-06 10:19:19 -0700

    7 The Homepage | Part 5 | Final touches on the layout

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -11,7 +11,7 @@ const Index = () => (
             
             {/* <Navbar /> */}
         </header>
-        <div className="grid-container">
+        <div className="grid-container grid-container--home">
             <div>
                 <h1 className="text-accent fs-500 ff-sans-cond uppercase letter-spacing-1">So, you want to travel to
                 <span className="d-block fs-900 ff-serif text-white">Space</span></h1>
