ea6376ccefe7bd1de6495dc7428a58a6734cd263
onnymm <phr2807@gmail.com>
2024-04-09 09:35:28 -0700

    3 The Destination Page | The Mobile Layout

--- src/ui_components/destination_description.jsx
+++ src/ui_components/destination_description.jsx
@@ -4,17 +4,17 @@ const DestinationDescription = ({useItem}) => {
     const destination = DATA.destinations.find(item => item.name === useItem);
 
     return (
-        <article>
-            <h2>{destination.name}</h2>
+        <article className="destination-info">
+            <h2 className="ff-serif fs-800 uppercase">{destination.name}</h2>
             <p>{destination.description}</p>
-            <div className="flex">
+            <div className="destination-meta flex">
                 <div>
-                    <h3>Avg. distance: </h3>
-                    <p>{destination.distance}</p>
+                    <h3 className="text-accent fs-200 uppercase">Avg. distance: </h3>
+                    <p className="fs-500 ff-serif uppercase">{destination.distance}</p>
                 </div>
                 <div>
-                    <h3>Est. travel time: </h3>
-                    <p>{destination.travel}</p>
+                    <h3 className="text-accent fs-200 uppercase">Est. travel time: </h3>
+                    <p className="fs-500 ff-serif uppercase">{destination.travel}</p>
                 </div>
             </div>
         </article>
