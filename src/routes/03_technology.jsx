77518dcfd00f64bda36a0840f54ae99760a42145
onnymm <phr2807@gmail.com>
2024-04-11 14:43:08 -0700

    Checkpoint

--- src/routes/03_technology.jsx
+++ src/routes/03_technology.jsx
@@ -8,17 +8,17 @@ const Technology = () => {
     const [useTechnology, setTechnology] = useState(data[0]);
 
     return (
-        <main id="main" className="technology grid-container grid-container--technology flow">
-            <h1 className="numbered-title"><span aria-hidden='true'>03</span>Technology</h1>
-            <div className="landscape">
-                <Cover name={`${useTechnology.name}-landscape`} type={'jpg'} />
-            </div>
-            <div className="portrait">
-                <Cover name={`${useTechnology.name}-portrait`} type={'jpg'} />
-            </div>
-            <NavigationNumber useItem={useTechnology} setItem={setTechnology} data={data}/>
-            <DescriptionTechnology name={useTechnology.name} description={useTechnology.description}/>
-        </main>
+            <main id="main" className="technology grid-container grid-container--technology flow">
+                <h1 className="numbered-title"><span aria-hidden='true'>03</span>Technology</h1>
+                <div className="landscape">
+                    <Cover name={`${useTechnology.name}-landscape`} type={'jpg'} />
+                </div>
+                <div className="portrait">
+                    <Cover name={`${useTechnology.name}-portrait`} type={'jpg'} />
+                </div>
+                <NavigationNumber useItem={useTechnology} setItem={setTechnology} data={data}/>
+                <DescriptionTechnology name={useTechnology.name} description={useTechnology.description}/>
+            </main>
     )
 }
 
